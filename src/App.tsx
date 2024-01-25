a2f1c9d1c0e9884ca9994945525ccbf14202cc35
dfjera <dfjera@dfjera.eu>
2024-01-25 17:48:02 +0100

    colors :P

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,48 @@
 import React, {useState} from 'react';
 import './App.css';
 
+import flags from "./flags.json";
+import colors from "./colormap.json"
+interface FlagProps {
+    type: string
+    colors: string[]
+}
+interface ColorsProp{
+    [key: string]: any
+}
+
+function getColor(colorname: string){
+    const cs: ColorsProp = colors.html;
+    if (Array.isArray(cs[colorname])) {
+        let [r, g, b] = cs[colorname];
+        return `rgb(${r}, ${g}, ${b})`;
+    }
+    return colorname;
+}
+
+function Flag(props: Readonly<{ flag: FlagProps }>) {
+    return <div className={"flag flag-vertical"}>
+        {props.flag.colors.map( (stripe,) => {
+            return <div style={{backgroundColor: getColor(stripe)}}/> // NOSONAR
+        })}
+    </div>;
+}
+
 function App() {
+    const [selectedFlag, setSelectedFlag] = useState<keyof typeof flags>("agender")
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <select value={selectedFlag} onChange={e=> {
+            // @ts-ignore
+            setSelectedFlag(e.target.value);
+        }}>{Object.keys(flags).map(k =>
+         <option key={k} value={k}>{k}</option>
+        )}</select>
       </header>
+        <main>
+            <Flag flag={flags[selectedFlag]}/>
+        </main>
     </div>
   );
 }
